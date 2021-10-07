7a5d128744d064dbc50321df628d9ea6909f21e0
Shahid Shafiq <mshahid@gmail.com>
2021-10-07 13:49:17 +0300

    facade/igiition

--- app/Http/Controllers/FeeController.php
+++ app/Http/Controllers/FeeController.php
@@ -14,10 +14,16 @@ class FeeController extends Controller
     //
     public function courses($cmpid) {
         // course classes at campus (cmpid)
+        /*
         $courses = Section::select('course_id')->where(['campus_id' => $cmpid])
         ->select(['course_id', 'courses.title', 'courses.description'])
         ->join('courses', 'course_id', '=', 'courses.id')->get();
         return $courses;
+        */
+
+        //return all courses and user is responsible for integrity
+        return Course::select(['id as course_id', 'title', 'description'])->get();
+
         //return Student::courses($cmpid);
     }
 
