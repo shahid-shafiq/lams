82bcda1282647324fbd9f814521e95c348003865
Shahid Shafiq <mshahid@gmail.com>
2022-08-29 18:34:35 +0500

    Fixes

--- app/Subincome.php
+++ app/Subincome.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subincome extends Model
 {
-    use HasFactory;
+    //use HasFactory;
     public $timestamps = false;
     public $table = 'subincome';
 
